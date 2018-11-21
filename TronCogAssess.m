5d59d9b196ef8bd53f4dc5243cc1926f5c865880
chimera2650 <33965509+chimera2650@users.noreply.github.com>
2018-11-21 14:24:41 -0800

    Further optimized cog assess code

--- TronCogAssess.m
+++ TronCogAssess.m
@@ -4,199 +4,200 @@ clear;
 clc;
 
 %% Define Variables
-prefix = 'CogAssess_flynn_';
+chan_count = 62; % Number of channels in analysis
+chan_name1 = 'FCz'; % Name of channel where effect occurs
+chan_name2 = 'Pz';
+cond_count = 2; % Number of conditions in analysis
+d_name = 'cog_assess.mat'; % Name of master data file
+prefix = 'CogAssess_flynn_'; % Prefix of raw data files
+s_rate = 4; % Sampling rate in milliseconds
+time_points = [-200 600]; % Desired time range for data
 comp = getenv('computername');
 
 if strcmp(comp,'JORDAN-SURFACE') == 1
-    working_dir = 'C:\Users\chime\Documents\MATLAB\MedEd\Data';
-    working_dir1 = 'C:\Users\chime\Documents\MATLAB\MedEd\Data\Cog Assess\RewP';
-    working_dir2 = 'C:\Users\chime\Documents\MATLAB\MedEd\Data\Cog Assess\P300';
-    save_path = 'C:\Users\chime\Documents\MATLAB\MedEd\Data\cog_assess.mat';
+    master_dir = 'C:\Users\chime\Documents\MATLAB\MedEd\Data';
+    rewp_dir = 'C:\Users\chime\Documents\MATLAB\MedEd\Data\Cog Assess\RewP';
+    p3_dir = 'C:\Users\chime\Documents\MATLAB\MedEd\Data\Cog Assess\P300';
+    save_dir = 'C:\Users\chime\Documents\MATLAB\MedEd\Data\cog_assess.mat';
 elseif strcmp(comp,'DESKTOP-U0FBSG7') == 1
-    working_dir = 'C:\Users\Jordan\Documents\MATLAB\MedEd\Data';
-    working_dir1 = 'C:\Users\Jordan\Documents\MATLAB\MedEd\Data\Cog Assess\RewP';
-    working_dir2 = 'C:\Users\Jordan\Documents\MATLAB\MedEd\Data\Cog Assess\P300';
-    save_path = 'C:\Users\Jordan\Documents\MATLAB\MedEd\Data\cog_assess.mat';
+    master_dir = 'C:\Users\Jordan\Documents\MATLAB\MedEd\Data';
+    rewp_dir = 'C:\Users\Jordan\Documents\MATLAB\MedEd\Data\Cog Assess\RewP';
+    p3_dir = 'C:\Users\Jordan\Documents\MATLAB\MedEd\Data\Cog Assess\P300';
+    save_dir = 'C:\Users\Jordan\Documents\MATLAB\MedEd\Data\cog_assess.mat';
 end
 
 clear comp
 
 %% Load Data
-cd(working_dir);
-load('cog_assess.mat');
+cd(master_dir);
+load(d_name);
+time_range = abs(max(time_points) - min(time_points));
+
+for b = 1:chan_count
+    if strcmp(summary.chanlocs(b).labels,chan_name1) == 1
+        c_loc(b) = 1;
+    elseif strcmp(summary.chanlocs(b).labels,chan_name2) == 1
+        c_loc(b) = 2;
+    else
+        c_loc(b) = 0;
+    end
+end
+
+clear a;
 
 %% ERP Analysis
-for y = 1:2
-    if y == 1
-        cd(working_dir1);
-    elseif y == 2
-        cd(working_dir2);
+for a = 1:2
+    if a == 1
+        analysis = 'rewp';
+        cd(rewp_dir);
+        chan_num = find(c_loc == 1);
+    elseif a == 2
+        analysis = 'p300';
+        cd(p3_dir);
+        chan_num = find(c_loc == 2);
     end
+    
     filenames = dir(strcat(prefix,'*'));
     file_num = length(filenames);
-    disp('Summarizing ERP by subject');
     
-    for x = 1:file_num
-        subject_data = importdata(filenames(x).name);
-        for i = 1:2
-            for ii = 1:62
-                for iii = 1:62
-                    if strcmpi(subject_data.chanlocs(iii).labels,cog_assess.chanlocs(ii).labels) == 1
-                        chan_loc(iii) = 1;
+    for b = 1:file_num
+        % First, data is collected by subject into a temporary array
+        sub_data = importdata(filenames(b).name);
+        for c = 1:cond_count
+            % Each array is divided into cells, one per condition
+            for d = 1:chan_count
+                % Each row in the cell corrosponds to a channel
+                for e = 1:chan_count
+                    % This normalizes the row number to ensure that all
+                    % channels corrospond to the same row
+                    if strcmpi(sub_data.chanlocs(e).labels,summary.chanlocs(d).labels) == 1
+                        chan_loc(e) = 1;
                     else
-                        chan_loc(iii) = 0;
+                        chan_loc(e) = 0;
                     end
                 end
                 c_index = find(chan_loc == 1);
-                temp_data(ii,:) = subject_data.ERP.data{i}(c_index,:);
+                temp_data{b}{c}(d,:) = sub_data.ERP.data{c}(c_index,:);
             end
-            summary_table{i} = temp_data;
         end
-        summary_data{x} = summary_table;
     end
     
-    disp('Combining subject ERP data');
-    
-    for x = 1:2
-        for i = 1:62
-            for ii = 1:file_num
-                temp_sub_sum(ii,:) = summary_data{ii}{x}(i,:); % Collapse by subject
-            end
-            temp_chan_sum(i,:) = mean(temp_sub_sum(:,:)); % Create a row for each channel
-        end
-        grand_summary{x} = temp_chan_sum; % Create a cell for each condition
-    end
-    
-    if y == 1
-        cog_assess.rewp.data = grand_summary;
-    elseif y == 2
-        cog_assess.p300.data = grand_summary;
-    end
-    
+    clear b;
+    clear c;
+    clear d;
+    clear e;
     clear c_index;
     clear chan_loc;
-    clear i;
-    clear ii;
-    clear iii;
-    clear subject_data;
-    clear summary_data;
-    clear summary_table;
-    clear temp_chan_sum;
+    clear d;
+    clear sub_data;
+    
+    disp('Combining ERP data by condition');
+    
+    for b = 1:cond_count
+        % Data is collapsed between subjects to create condition averages
+        for c = 1:chan_count
+            for d = 1:file_num
+                temp_sum(d,:) = temp_data{d}{b}(c,:);
+            end
+            sum_data{b}(c,:) = nanmean(temp_sum(:,:));
+        end
+    end
+    
+    summary.(analysis).data = sum_data;
+    
+    clear b;
+    clear c;
+    clear d;
+    clear temp_sum;
+    
+    % Standard Deviation for ERP
+    disp('Calculating ERP standard deviations');
+    
+    for b = 1:cond_count
+        % Data is collapsed between subjects to create condition standard
+        % deviations
+        for c = 1:chan_count
+            for d = 1:file_num
+                temp_sum(d,:) = temp_data{d}{b}(c,:);
+            end
+            sum_data{b}(c,:) = nanstd(temp_sum(:,:));
+        end
+    end
+    
+    summary.(analysis).std = sum_data;
+    
+    clear b;
+    clear c;
+    clear d;
+    clear sum_data;
     clear temp_data;
-    clear temp_sub_sum;
-    clear x;
+    clear temp_sum;
     
-    disp('Running ERP descriptive stats by subject');
+    % ANOVA
+    disp('Calculating between-subject ANOVAs for ERP');
     
-    for x = 1:file_num
-        subject_data = importdata(filenames(x).name);
-        for i = 1:2
-            for ii = 1:62
-                for iii = 1:62
-                    if strcmpi(subject_data.chanlocs(iii).labels,cog_assess.chanlocs(ii).labels) == 1
-                        chan_loc(iii) = 1;
-                    else
-                        chan_loc(iii) = 0;
-                    end
-                end
-                c_index = find(chan_loc == 1);
-                temp_data(ii,:) = subject_data.ERP.data{i}(c_index,:);
-            end
-            summary_table{i} = temp_data;
-        end
-        summary_data{x} = summary_table;
-    end
-    
-    disp('Combining descriptive stats for ERP');
-    
-    for x = 1:2
-        for i = 1:62
-            for ii = 1:file_num
-                temp_sub_sum(ii,:) = summary_data{ii}{x}(i,:); % Collapse by subject
-            end
-            temp_chan_sum(i,:) = std(temp_sub_sum(:,:)); % Create a row for each channel
-        end
-        grand_summary{x} = temp_chan_sum; % Create a cell for each condition
-    end
-    
-    if y == 1
-        cog_assess.rewp.std = grand_summary;
-    elseif y == 2
-        cog_assess.p300.std = grand_summary;
-    end
-    
-    summary_table = [];
-    
-    disp('Running ERP ANOVAs');
-    
-    for x = 1:file_num
-        subject_data = importdata(filenames(x).name);
-        temp_data1 = transpose(subject_data.ERP.data{1}(c_index,:));
-        temp_data2 = transpose(subject_data.ERP.data{2}(c_index,:));
-        summary_data1(:,1) = temp_data1;
-        summary_data1(:,2) = temp_data2;
+    for b = 1:file_num
+        sub_data = importdata(filenames(b).name);
+        temp_data(:,1) = transpose(sub_data.ERP.data{1}(chan_num,:));
+        temp_data(:,2) = transpose(sub_data.ERP.data{2}(chan_num,:));
         
-        for i = 1:length(summary_data1)
-            summary_data2(i,:) = mean(summary_data1(i,:));
+        for c = 1:length(temp_data)
+            temp_sum = nanmean(temp_data(c,:),1);
         end
         
-        summary_table(end+1,:) = transpose(summary_data2);
+        sum_data(b,:) = transpose(temp_sum);
     end
     
-    cond1 = isnan(summary_table(:,1));
-    summary_table(cond1,:) = [];
-    summary_table = transpose(summary_table);
+    clear b;
+    clear c;
+    clear sub_data;
+    clear temp_data;
+    clear temp_sum;
     
-    [p,tbl,stats] = anova1(summary_table,[],'off');
-    ERPanova.p = p;
-    ERPanova.tbl = tbl;
-    ERPanova.stats = stats;
+    % Remove empty rows
+    nan_rows = isnan(sum_data(:,1));
+    sum_data(nan_rows,:) = [];
+    sum_data = transpose(sum_data);
     
-    if y == 1
-        cog_assess.rewp.anova = grand_summary;
-    elseif y == 2
-        cog_assess.p300.anova = grand_summary;
-    end
+    % Calculate ANOVA
+    [p,tbl,stats] = anova1(sum_data,[],'off');
+    sum_anova.p = p;
+    sum_anova.tbl = tbl;
+    sum_anova.stats = stats;
+    summary.(analysis).anova = sum_anova;
     
-    clear c_index;
-    clear cond1;
-    clear ERPanova;
-    clear i;
+    clear chan_num;
+    clear nan_rows;
     clear p;
     clear stats;
-    clear subject_data;
-    clear summary_data1;
-    clear summary_data2;
-    clear summary_table;
+    clear sub_data;
+    clear sum_anova;
+    clear sum_data;
     clear tbl;
-    clear temp_data;
-    clear temp_data1;
-    clear temp_data2;
-    clear x;
 end
 
-for x = 1:200
-    time_point(1,x) = (-200+(4*x));
+clear a;
+clear c_loc;
+
+%% Create a table for time points; used in plotting data
+disp('Creating timepoints for ERP');
+
+for a = 1:(time_range/s_rate)
+    t_point(1,a) = (min(time_points)+(s_rate*a));
 end
 
-cog_assess.time = time_point;
+summary.ERP.time = t_point;
 
-clear c_index;
-clear chan_loc;
-clear filenames;
-clear grand_summary;
-clear i;
-clear ii;
-clear iii;
-clear subject_data;
-clear summary_data;
-clear summary_table;
-clear temp_chan_sum;
-clear temp_data;
-clear temp_sub_sum;
-clear time_point;
-clear x;
-clear y;
+clear a;
+clear t_point;
+clear time_points;
+clear time_range;
 
 %% Save Data
-save(save_path,'cog_assess');
+disp('Saving data');
+save(save_dir,'summary');
+
+%% Final Cleanup
+disp('Analysis complete');
+
+clearvars -except summary;
